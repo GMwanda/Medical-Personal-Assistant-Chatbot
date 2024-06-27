4d34a8dbb5d7908ccab45aa5cff2cdee5d01768d
GMwanda <george.mwanda@strathmore.edu>
2024-06-27 09:52:15 +0300

    Completed Chatbot with basic greetings

--- training.py
+++ training.py
@@ -6,11 +6,13 @@ import numpy as np
 import nltk
 
 nltk.download('punkt')
+nltk.download('wordnet')
 from nltk.stem import WordNetLemmatizer
 
-# from tensorflow.keras.models import Sequential
-# from tensorflow.keras.layers import Dense, Activation, Dropout
-# from tensorflow.keras.optimizers import SGD
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Activation, Dropout
+from tensorflow.keras.optimizers import SGD
+
 
 lemmatizer = WordNetLemmatizer()
 
@@ -29,15 +31,12 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
-print(documents)
-
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
 words = sorted(set(words))
-print(words)
 classes = sorted(set(classes))
 
 pickle.dump(words, open('words.pkl', 'wb'))
-pickle.dump(words, open('classes.pkl', 'wb'))
+pickle.dump(classes, open('classes.pkl', 'wb'))  # Fixed typo, was 'words.pkl' instead of 'classes.pkl'
 
 training = []
 output_empty = [0] * len(classes)
@@ -54,7 +53,24 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+training = np.array(training, dtype=object)  # Set dtype to object to handle sequences
 
-train_x = list(training[:, 0])
-train_y = list(training[:, 1])
+train_x = np.array([item[0] for item in training])
+train_y = np.array([item[1] for item in training])
+
+from tensorflow.keras.layers import Input
+
+model = Sequential()
+model.add(Input(shape=(len(train_x[0]),)))
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation='softmax'))
+
+sgd = SGD(learning_rate=0.01, momentum=0.9, nesterov=True)  # Updated argument names
+model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+
+model.fit(train_x, train_y, epochs=200, batch_size=5, verbose=1)
+model.save('chatbot_model.keras')
+print('Done')
